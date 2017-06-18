e2804b2056af9d2de4140ddea952157b158ba9c1
Andrew Yang <ayang@andrews-air>
2017-06-17 22:26:32 -0500

    Initial commit.

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var labelTopLeft: GridLabel!
+    @IBOutlet weak var labelTopCenter: GridLabel!
+    @IBOutlet weak var labelTopRight: GridLabel!
+    @IBOutlet weak var labelCenterLeft: GridLabel!
+    @IBOutlet weak var labelCenterCenter: GridLabel!
+    @IBOutlet weak var labelCenterRight: GridLabel!
+    @IBOutlet weak var labelBottomLeft: GridLabel!
+    @IBOutlet weak var labelBottomCenter: GridLabel!
+    @IBOutlet weak var labelBottomRight: GridLabel!
+    
+    var labelsArray = [GridLabel]()
+    var xTurn = true
+    var canTap = true
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        labelsArray = [labelTopLeft, labelTopCenter, labelTopRight, labelCenterLeft, labelCenterCenter, labelCenterRight, labelBottomLeft, labelBottomCenter, labelBottomRight]
+    }
+    
+    func setXorO(sender : UITapGestureRecognizer, text : String)
+    {  
+        for label in labelsArray
+        {
+            if (label.frame.contains(sender.location(in: backgroundView)))
+                && label.canTap == true
+            {
+                label.text = text
+                label.canTap = false
+                xTurn = !xTurn
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onTappedGridLabel(_ sender: UITapGestureRecognizer)
+    {
+        print("Tap executed.")
+
+        if xTurn == true
+        {
+            setXorO(sender: sender, text : "X")
+        }
+        else
+        {
+            setXorO(sender: sender, text : "O")
+        }
     }
-
-
 }
 
